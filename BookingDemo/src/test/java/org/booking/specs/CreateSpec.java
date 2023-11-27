59c45d11d0a22cff233623e0d4e451348f434b8d
Amar Causevic <amar.causevic01@gmail.com>
2023-11-27 09:07:29 +0100

    Added Logging Framework!
    Initial logging framework and Exception catching!

--- BookingDemo/src/test/java/org/booking/specs/CreateSpec.java
+++ BookingDemo/src/test/java/org/booking/specs/CreateSpec.java
@@ -7,34 +7,30 @@ import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import org.booking.actions.CreateDAO;
 import org.booking.model.CreateResponseDTO;
-import org.junit.Assert;
 
 public class CreateSpec extends CreateDAO {
 
-  private static CreateResponseDTO getCreateDetails;
+  private static Response response;
+  private static CreateResponseDTO createResponseDTO;
 
   @Given("user sends a CREATE request to the endpoint")
   public static void requestCreateBooking() {
-    Response response = createBooking();
-
-    getCreateDetails = getCreateResponse(response);
-    System.out.println(
-        "This is from inside the response: " + getCreateDetails.getBooking().getDepositPaid());
+    response = createBooking();
+    createResponseDTO = getCreateResponse(response);
   }
 
   @When("endpoint is reached, new booking is added to database")
   public static void validateResponseIsNotNull() {
-
+    validateNewBookingIsCreated(createResponseDTO);
   }
 
   @Then("validate correct values are added to database")
   public static void validateResponseData() {
-
+    
   }
 
   @And("validate create status code {int}")
   public static void validateEndResponseStatus(int defaultStatusCode) {
-    //Change this get value from response!
-    Assert.assertEquals(defaultStatusCode, 200);
+    assertStatusCode(response, defaultStatusCode);
   }
 }
